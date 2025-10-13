742c455e81212fde10a66ae3f2a23bb3f35a5bfb
KennFatt <kennfatt@gmail.com>
2025-10-13 09:52:29 +0700

    feat(dev): updated `aliases.sh`

--- dev/aliases.sh
+++ dev/aliases.sh
@@ -4,3 +4,9 @@ alias src-asm='gcc -S $1 -o _src_asm.s'
 
 # Code with code-insiders
 alias code-here="code-insiders . &; sleep 0.5; disown; exit;"
+
+# Android emulator
+if [[ -d $ANDROID_HOME ]]; then
+	alias android-device-list='emulator -list-avds'
+	alias android-device-run='emulator -avd $1'
+fi
