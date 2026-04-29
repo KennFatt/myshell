3f70c1784db3f198fa81f13e2022c27be0a46a2e
KennFatt <kennfatt@gmail.com>
2026-04-29 10:36:58 +0700

    feat(git): added new alias

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,3 +1,4 @@
 alias git-most-active="git log --format=format: --name-only --since=12.month| egrep -v '^$' | sort | uniq -c | sort -nr | head -50"
 alias git-uncommit="git reset --soft HEAD~1"
 alias git-get-head="git rev-parse HEAD"
+alias git-staged="git diff --name-only --cached"
