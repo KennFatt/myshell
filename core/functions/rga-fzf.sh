aef1362ca9b817d48e0ef8f038c759f8479b0573
KennFatt <kennfatt@gmail.com>
2026-05-12 19:22:38 +0700

    feat(core): override `rga-fzf` bin with manual shell func

---
+++ core/functions/rga-fzf.sh
@@ -0,0 +1,16 @@
+rga-fzf() {
+  RG_PREFIX="rga --files-with-matches"
+
+  local file
+  file="$(
+    FZF_DEFAULT_COMMAND="$RG_PREFIX '$*'" \
+    fzf \
+      --phony \
+      --query "$*" \
+      --bind "change:reload:$RG_PREFIX {q} || true" \
+      --preview "rga --pretty --context 20 {q} {}" \
+      --preview-window "right:50%:wrap"
+  )" || return
+
+  printf '%s\n' "$file"
+}
